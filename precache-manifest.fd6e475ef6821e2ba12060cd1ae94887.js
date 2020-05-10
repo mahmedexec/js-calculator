bd310074ae3cf52f0147b9861c9137cf9e9194d3
Ahmed Tan <ahmedtan@ahmeds-mbp.home>
2020-05-10 17:44:22 +0500

    Updates

---
+++ precache-manifest.fd6e475ef6821e2ba12060cd1ae94887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fce5b2755faaca1b807ab7c8bf3c636",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "26350e25d09d0a57b8d6",
+    "url": "/js-calculator/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "559cf910e096de0e627b",
+    "url": "/js-calculator/static/css/main.4a06d0fd.chunk.css"
+  },
+  {
+    "revision": "26350e25d09d0a57b8d6",
+    "url": "/js-calculator/static/js/2.d0172cb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-calculator/static/js/2.d0172cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559cf910e096de0e627b",
+    "url": "/js-calculator/static/js/main.fb1a5406.chunk.js"
+  },
+  {
+    "revision": "7a55f954100598831dc0",
+    "url": "/js-calculator/static/js/runtime-main.9845acf8.js"
+  }
+]);
