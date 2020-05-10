01df316a797b175512830583f8c00d7119e1ffa4
Ahmed Tan <ahmedtan@ahmeds-mbp.home>
2020-05-10 17:31:20 +0500

    Final draft

--- src/App.js
+++ src/App.js
@@ -20,38 +20,57 @@ class App extends React.Component {
       first: true,
     });
   };
-
+  
   handleClick = (event) => {
-    this.setState({
-      input: this.state.input.concat(event.target.innerHTML),
-      first: false,
-    });
+    if (
+      this.state.input.charAt(0) == 0 &&
+      this.state.first == false &&
+      event.target.innerHTML == "0"
+    )
+      return;
+    if (
+      this.state.input.charAt(this.state.input.indexOf(".") >= 0) == "." &&
+      this.state.first == false &&
+      event.target.innerHTML == "."
+    )
+      return;
+     
+        
+    else {
+      this.setState({
+        input: this.state.input.concat(event.target.innerHTML),
+        first: false,
+      });
+    }
   };
   handleOutput = (event) => {
     var numbersArr = this.state.input.match(/\d{1,10}[\.?\d{1,10}]*/g);
-    var operationsArr = this.state.input.match(/(?!^-)[+*\/-](\s?-)?/g);
+    var operationsArr = this.state.input
+      .match(/[x+-/]/g)
+      .filter((item) => item != ".");
 
     let total = parseFloat(numbersArr[0]);
 
     console.log(numbersArr);
     console.log(operationsArr);
-
+    if (numbersArr.length < operationsArr.length) {
+      operationsArr.splice(0,operationsArr.length-1);
+    }
     for (let i = 1; i < numbersArr.length; i++) {
-      switch (operationsArr[i-1]) {
+      switch (operationsArr[i - 1]) {
         case "+":
           console.log("+");
-          
           total += parseFloat(numbersArr[i]);
           break;
         case "-":
           console.log("-");
 
-          total -=  parseFloat(numbersArr[i]);
+          total -= parseFloat(numbersArr[i]);
           break;
         case "x":
           console.log("x");
 
-          total *=parseFloat(numbersArr[i]);
+          total *= parseFloat(numbersArr[i]);
           break;
         case "/":
           console.log("/");
@@ -59,23 +78,22 @@ class App extends React.Component {
           total /= parseFloat(numbersArr[i]);
           break;
       }
-      console.log(total +"   turn    "+i);
-      
-    }
-    this.setState({
-      input:total.toString(),
-    })
-    console.log(total);
-    
-  };
 
+      console.log(total + "   turn    " + i);
+    }
+    if (numbersArr.length == operationsArr.length && operationsArr.indexOf("-")>=0) total = -total;
+    this.setState({
+      input: total.toString(),
+    });
+    console.log(total);
+  };
 
   render() {
     return (
       <div className="container-fluid fill-viewport">
         <h1 className="title text text-responsive text-center ">Calculator</h1>
-
-        <div className="row d-align-items-center justify-content-center">
+<div className="container bg bg-warning col-lg-6">
+        <div className="row ">
           <h1 id="display" className=" p-3 rounded-lg col-9 bg-warning ">
             {this.state.first ? 0 : this.state.input}
           </h1>
@@ -209,6 +227,7 @@ class App extends React.Component {
           </button>
           <div className="w-100"></div>
         </div>
+      </div>
       </div>
     );
   }
